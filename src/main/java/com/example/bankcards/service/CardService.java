ca1407dde90576239847635e6fc43cfb685f510e
Maksim Piva <m.piva@mail.astondevs.ru>
2025-10-23 10:12:46 +0300

    task-service добавление сервисов

---
+++ src/main/java/com/example/bankcards/service/CardService.java
@@ -0,0 +1,10 @@
+package com.example.bankcards.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CardService {
+
+}
